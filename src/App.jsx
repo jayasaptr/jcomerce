e17a799ed680d23298f1f5e3067fdfedada00537
ClaraMariaSaputra <claramariaangelinaoraplean553@gmail.com>
2024-05-11 07:14:32 +0800

    feat:[MJ] learn state

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Box } from "./components/Box";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
@@ -9,14 +11,14 @@ const productRaw = [
     image:
       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1419ed34-7762-420a-9f2a-2e2a65d5aba9/dunk-low-shoes-kKHp1z.png",
     price: 250000,
-    stock: 10,
+    stock: 3,
   },
   {
     name: "Nike Jordan Mid",
     image:
       "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/63193ad7-3dcb-4ef9-a242-77126431b022/air-jordan-1-elevate-low-shoes-XlkVrM.png",
     price: 150000,
-    stock: 15,
+    stock: 0,
   },
 ];
 
@@ -34,17 +36,17 @@ function App() {
   return (
     <>
       <Header />
-      <main className="min-h-[90vh] max-w-screen-md mx-auto px-4 mt-8">
-        <div className="pb-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+      <main className='min-h-[90vh] max-w-screen-md mx-auto px-4 mt-8'>
+        <div className='pb-20 mx-auto text-center flex flex-col items-center max-w-3xl'>
+          <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
             Become a trand-setter with us.
           </h1>
-          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          <p className='mt-6 text-lg max-w-prose text-muted-foreground'>
             Jproject provides you with the finest clothings and ensures your
             confidence throghout your days
           </p>
         </div>
-        <div className="grid grid-cols-2 gap-4">{product}</div>
+        <div className='grid grid-cols-2 gap-4'>{product}</div>
       </main>
       <Footer />
     </>
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,16 +1,73 @@
+/** @format */
+
+import { Button } from "./ui/button";
+import { IoIosAdd, IoIosRemove } from "react-icons/io";
+import { useState } from "react";
+
 export const ProductCard = ({ image, name, price, stock }) => {
+  // let quantity = 0;
+
+  // const [message, setMessage] = useState("add to cart");
+
+  const [quantity, setQuantity] = useState(0);
+
+  const addToCart = () => {
+    setMessage("mantap");
+  };
+
+  const incrementQuantity = () => {
+    if (quantity < stock) {
+      setQuantity(quantity + 1);
+    }
+  };
+
+  const decrementQuantity = () => {
+    if (quantity != 0) {
+      setQuantity(quantity - 1);
+    }
+  };
   return (
-    <div className="p-4 border rounded-md md:max-w-96 flex flex-col gap-4">
-      <div className="aspect-square w-full overflow-hidden">
-        <img className="w-full" src={image} alt="product" />
+    <div className='p-4 border rounded-md md:max-w-96 flex flex-col gap-4'>
+      <div className='aspect-square w-full overflow-hidden'>
+        <img className='w-full' src={image} alt='product' />
       </div>
 
       <div>
-        <p className="text-base">{name}</p>
-        <p className="text-xl font-semibold">
+        <p className='text-base'>{name}</p>
+        <p className='text-xl font-semibold'>
           Rp {price.toLocaleString("id-ID")}
         </p>
-        <p className="text-muted-foreground text-sm">In Stock: {stock}</p>
+        <p className='text-muted-foreground text-sm'>In Stock: {stock}</p>
+      </div>
+      <div className='flex flex-col gap-2'>
+        {/* Button Quantity */}
+        <div className='flex justify-between items-center'>
+          <Button
+            disabled={quantity === 0}
+            onClick={decrementQuantity}
+            size='icon'
+            variant='ghost'>
+            <IoIosRemove
+              className='
+            h-6 w-6'
+            />
+          </Button>
+          <p className='text-lg font-bold'>{quantity}</p>
+          <Button
+            disabled={quantity >= stock}
+            onClick={incrementQuantity}
+            size='icon'
+            variant='ghost'>
+            <IoIosAdd
+              className='h-6 w-6
+            '
+            />
+          </Button>
+        </div>
+        {/* Button add to cart */}
+        <Button disabled={!stock} onClick={addToCart} className='w-full'>
+          {stock > 0 ? "Add to cart" : "Out of stock"}
+        </Button>
       </div>
     </div>
   );
